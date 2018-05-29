18b668de3f19fc719a7c5a34a436fee589a242e8
jmtabo02 <judson.tabor@stu.jefferson.kyschools.us>
2018-05-29 07:50:54 -0400

    project alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,32 @@
 import sys
 import pygame
-from settings import Settings
-
-
+from general_settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-            screen.fill(ai_settings.bg_color)
-
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
+        bullets.update()
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
 
 
 run_game()
